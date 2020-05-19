4e56a001e9a5e15cdced1ea6ae94025fa3465573
morlinski <michal.orlinski@whirly.pl>
2020-05-19 17:46:29 +0200

    Example OpenShift Web Application

---
+++ src/main/java/pl/morlinski/wjug/wjug265/HelloController.java
@@ -0,0 +1,14 @@
+package pl.morlinski.wjug.wjug265;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
